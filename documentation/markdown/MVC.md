f86118c378586db97de8a6268c632b7fac7b5728
Leo-Zubiri <leo.zubiri@outlook.com>
2022-10-11 21:23:45 -0500

    11-10-2022

---
+++ documentation/markdown/MVC.md
@@ -0,0 +1,7 @@
+## **MVC - MODEL VIEW CONTROLLER**
+
+**Modelo** - Es el encargado con todas las interacciones en la base de datos.
+
+**Vista** - Se encarga de todo lo que se ve en pantalla.
+
+**Controlador** - Comunica modelo y vista. 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,10 +7,28 @@
         @vite('resources/css/app.css')
     </head>
     <body class="antialiased">
+        
+        <header class="p-5 border-b bg-white shadow">
+            <div class="conatainer mx-auto flex justify-between items-center">
+                <h1 class="text-3xl font-black">Devstagram</h1>
 
-        <h1 class="text-4xl font-extrabold"> @yield('titulo')</h1>
+                <nav class="flex gap-2 items-center">
+                    <a class="font-bold uppercase text-gray-600" href="#">Login</a>
+                    <a class="font-bold uppercase text-gray-600" href="/crear-cuenta">
+                        Crear Cuenta
+                    </a>
+                </nav>
+            </div>
+        </header>
 
-        @yield('contenido')
+        <main class="container mx-auto mt-10">
+            <h2 class="font-black text-center text-3xl mb-10">@yield('titulo')</h2>
+            @yield('contenido')
+        </main>
+        
+        <footer class="text-center p-5 text-gray-500 font-bold uppercase">
+            Devstagram - Todos los derechos reservados  {{ now()->year }}
+        </footer>
 
     </body>
 </html>
