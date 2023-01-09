baaba7909f4e9802f0c5bcbaddd3d441a2e90643
Leo-Zubiri <leo.zubiri@outlook.com>
2023-01-08 22:16:46 -0600

    Modelos y relaciones

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,8 +5,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Devstagram - @yield('titulo')</title>
-    @vite('resources/css/app.css')`
-    @vite('resources/js/app.js')`
+    @vite('resources/css/app.css')
+    @vite('resources/js/app.js')
     @stack('styles')
 </head>
 
