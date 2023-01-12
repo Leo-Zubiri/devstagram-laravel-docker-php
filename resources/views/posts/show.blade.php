49cc59210cee888dc80fc96010f113ea52d8e9b6
Leo-Zubiri <leo.zubiri@outlook.com>
2023-01-11 23:57:09 -0600

    Comentario

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -5,7 +5,7 @@
 @endsection
 
 @section('contenido')
-    <div class="container mx-auto flex">
+    <div class="container mx-auto md:flex">
         <div class="md:w-1/2">
             <img 
                 src="{{asset('uploads').'/'.$post->imagen}}" 
@@ -30,7 +30,9 @@
                 @auth
                 <p class="text-xl font-bold text-center mb-4"> Agrega un nuevo comentario</p>
 
-                <form action="">
+                <form action="{{ route('comentarios.store',['post'=>$post, 'user'=>$user]) }}"
+                    method="POST">
+                    @csrf
                     <div class="mb-5">
                         <label for="comentario"
                         class="mb-2 block uppercase text-gray-500 font-bold">
