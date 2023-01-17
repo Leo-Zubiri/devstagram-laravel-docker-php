1c291390513f057c6d9859fd1aa6ae7434747cee
Leo-Zubiri <leo.zubiri@outlook.com>
2023-01-16 23:16:45 -0600

    Modelo Follower

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@
 */
 
 use App\Http\Controllers\ComentarioController;
+use App\Http\Controllers\FollowerController;
 use App\Http\Controllers\ImagenController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\LogoutController;
@@ -54,3 +55,7 @@ Route::post('/posts/{post}/likes',[LikeController::class,'store'])->name('posts.
 Route::delete('/posts/{post}/likes',[LikeController::class,'destroy'])->name('posts.likes.destroy');
 
 Route::get('/{user:username}',[PostController::class,'index'])->name('posts.index');
+
+// Siguiendo a usuarios
+Route::post('/{user:username}/follow',[FollowerController::class,'store'])->name('users.follow');
+Route::delete('/{user:username}/unfollow',[FollowerController::class,'destroy'])->name('users.unfollow');
