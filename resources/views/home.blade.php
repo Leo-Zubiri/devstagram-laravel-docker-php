3dca69c7eaa5c0fd4b11d6064acda0754d76f447
Leo-Zubiri <leo.zubiri@outlook.com>
2023-01-19 22:49:49 -0600

    Componentes en laravel

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,19 +5,5 @@
 @endsection
 
 @section('contenido')
-    <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-
-    @forelse ($posts as $post)
-        <div>
-            <a href="{{route('posts.show',['post'=>$post, 'user'=>$post->user])}}"
-            ><img src="{{ asset('uploads').'/'. $post->imagen }}" alt="Imagen Post {{ $post->titulo }}"></a>
-        </div>
-    @empty
-        <p class="text-center">No hay posts</p>
-    @endforelse
-
-    </div>
-    <div class="my-10">
-        {{ $posts->links('pagination::tailwind') }}
-    </div>
+    <x-listar-post :posts="$posts"/>
 @endsection
